dc7e97dfe58ed0b03940f88f5b774ae02e651e7f
iffiuszabolcsmikes <iffiu.szabolcs@mikeslici.ro>
2026-06-10 10:56:14 +0300

    Update EinsteinRejtveny.cpp

--- Backtracking/EinsteinRejtveny.cpp
+++ Backtracking/EinsteinRejtveny.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <iomanip>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -52,60 +52,76 @@ void kiir(int szin[],
 {
     const int W = 15;
 
-    cout << "\n";
+    cout << setw(W) << " ";
+    for(int i=1;i<=5;i++)
+        cout << setw(W) << i;
+    cout << endl;
 
-    cout << "+" << string(12,'-');
+    cout << setw(W) << "Szin";
     for(int i=0;i<5;i++)
-        cout << "+" << string(W,'-');
-    cout << "+\n";
+        cout << setw(W) << szinNev[szin[i]];
+    cout << endl;
 
-    cout << "|" << setw(12) << left << "";
-
+    cout << setw(W) << "Nemzet";
     for(int i=0;i<5;i++)
-        cout << "|" << setw(W) << left << i+1;
+        cout << setw(W) << nemzetNev[nemzet[i]];
+    cout << endl;
 
-    cout << "|\n";
-
-    cout << "+" << string(12,'-');
+    cout << setw(W) << "Ital";
     for(int i=0;i<5;i++)
-        cout << "+" << string(W,'-');
-    cout << "+\n";
+        cout << setw(W) << italNev[ital[i]];
+    cout << endl;
 
-    cout << "|" << setw(12) << left << "Szin";
+    cout << setw(W) << "Cigaretta";
     for(int i=0;i<5;i++)
-        cout << "|" << setw(W) << left << szinNev[szin[i]];
-    cout << "|\n";
+        cout << setw(W) << cigiNev[cigi[i]];
+    cout << endl;
 
-    cout << "|" << setw(12) << left << "Nemzet";
+    cout << setw(W) << "Allat";
     for(int i=0;i<5;i++)
-        cout << "|" << setw(W) << left << nemzetNev[nemzet[i]];
-    cout << "|\n";
-
-    cout << "|" << setw(12) << left << "Ital";
-    for(int i=0;i<5;i++)
-        cout << "|" << setw(W) << left << italNev[ital[i]];
-    cout << "|\n";
-
-    cout << "|" << setw(12) << left << "Cigaretta";
-    for(int i=0;i<5;i++)
-        cout << "|" << setw(W) << left << cigiNev[cigi[i]];
-    cout << "|\n";
-
-    cout << "|" << setw(12) << left << "Allat";
-    for(int i=0;i<5;i++)
-        cout << "|" << setw(W) << left << allatNev[allat[i]];
-    cout << "|\n";
-
-    cout << "+" << string(12,'-');
-    for(int i=0;i<5;i++)
-        cout << "+" << string(W,'-');
-    cout << "+\n";
+        cout << setw(W) << allatNev[allat[i]];
+    cout << endl;
 
     int halasHaz = hol(allat, HAL);
 
-    cout << "\nA halat a "
+    cout << endl << "A halat a "
          << nemzetNev[nemzet[halasHaz]]
-         << " tartja.\n";
+         << " tartja." << endl;
+}
+
+bool kovPerm(int a[], int n){
+    int piv;
+    int i;
+    for(i=n-1; i>0; i--){
+        if(a[i]>a[i-1]){
+            piv=i - 1;
+            break;
+        }
+    }
+    if(i==0)
+    {
+        int j = n - 1;
+        while(i < j)
+        {
+            swap(a[i++], a[j--]);
+        }
+        return false;
+    }
+
+    for(i=n-1; i>piv; i--){
+        if(a[i]>a[piv]){
+            swap(a[i], a[piv]);
+            break;
+        }
+    }
+    i = piv+1;
+    int j = n - 1;
+    while(i < j)
+    {
+        swap(a[i++], a[j--]);
+    }
+
+    return true;
 }
 
 int main()
@@ -116,83 +132,90 @@ int main()
     int cigi[]   = {0,1,2,3,4};
     int allat[]  = {0,1,2,3,4};
 
-    do
+    do //szin
     {
-        int zold = hol(szin,ZOLD);
-        int feher = hol(szin,FEHER);
+        int piros = hol(szin, PIROS);
+        int zold = hol(szin, ZOLD);
+        int feher = hol(szin, FEHER);
+        int sarga = hol(szin, SARGA);
+        int kek = hol(szin, KEK);
 
-        if(zold + 1 != feher)
+        if(feher-zold!=1)
             continue;
 
-        int kek = hol(szin,KEK);
-
-        do
+        do //nemzet
         {
-            if(hol(nemzet,NORVEG)!=0)
+            int brit= hol(nemzet, BRIT);
+            int dan = hol(nemzet, DAN);
+            int sved = hol(nemzet, SVED);
+            int norveg = hol(nemzet, NORVEG);
+            int nemet = hol(nemzet, NEMET);
+
+            if(norveg!=0)
+                continue;
+            if(abs(norveg-kek)!=1)
+                continue;
+            if(brit!=piros)
                 continue;
 
-            if(abs(hol(nemzet,NORVEG)-kek)!=1)
-                continue;
-
-            if(hol(nemzet,BRIT)!=hol(szin,PIROS))
-                continue;
-
-            do
+            do //ital
             {
-                if(hol(ital,TEJ)!=2)
+                int tea= hol(ital, TEA);
+                int kave= hol(ital, KAVE);
+                int viz= hol(ital, VIZ);
+                int sor= hol(ital, SOR);
+                int tej= hol(ital, TEJ);
+
+                if(dan!=tea)
+                    continue;
+                if(zold!=kave)
+                    continue;
+                if(tej!=2)
                     continue;
 
-                if(hol(nemzet,DAN)!=hol(ital,TEA))
-                    continue;
-
-                if(hol(szin,ZOLD)!=hol(ital,KAVE))
-                    continue;
-
-                do
+                do // cigi
                 {
-                    if(hol(szin,SARGA)!=hol(cigi,DUNHILL))
+                    int pallmall= hol(cigi, PALLMALL);
+                    int dunhill= hol(cigi, DUNHILL);
+                    int blends= hol(cigi, BLENDS);
+                    int prince= hol(cigi, PRINCE);
+                    int bluemaster= hol(cigi, BLUEMASTER);
+
+                    if(sarga!=dunhill)
+                        continue;
+                    if(sor!=bluemaster)
+                        continue;
+                    if(nemet!=prince)
+                        continue;
+                    if(abs(blends-viz)!=1)
                         continue;
 
-                    if(hol(cigi,BLUEMASTER)!=hol(ital,SOR))
-                        continue;
-
-                    if(hol(nemzet,NEMET)!=hol(cigi,PRINCE))
-                        continue;
-
-                    if(abs(hol(cigi,BLENDS)-hol(ital,VIZ))!=1)
-                        continue;
-
-                    do
+                    do //allat
                     {
-                        if(hol(nemzet,SVED)!=hol(allat,KUTYA))
+                        int kutya= hol(allat, KUTYA);
+                        int macska= hol(allat, MACSKA);
+                        int madar= hol(allat, MADAR);
+                        int lo= hol(allat, LO);
+                        int hal= hol(allat, HAL);
+
+                        if(sved!=kutya)
+                            continue;
+                        if(pallmall!=madar)
+                            continue;
+                        if(abs(blends-macska)!=1)
+                            continue;
+                        if(abs(lo-dunhill)!=1)
+                            continue;
+                        if(nemet!=prince)
                             continue;
 
-                        if(hol(cigi,PALLMALL)!=hol(allat,MADAR))
-                            continue;
+                        kiir(szin, nemzet, ital, cigi, allat);
 
-                        if(abs(hol(cigi,BLENDS)-hol(allat,MACSKA))!=1)
-                            continue;
-
-                        if(abs(hol(allat,LO)-hol(cigi,DUNHILL))!=1)
-                            continue;
-
-                        kiir(szin,nemzet,ital,cigi,allat);
-                        return 0;
-
-                    }
-                    while(next_permutation(allat,allat+5));
-
-                }
-                while(next_permutation(cigi,cigi+5));
-
-            }
-            while(next_permutation(ital,ital+5));
-
-        }
-        while(next_permutation(nemzet,nemzet+5));
-
-    }
-    while(next_permutation(szin,szin+5));
+                    }while(kovPerm(allat, 5));
+                }while(kovPerm(cigi, 5));
+            }while(kovPerm(ital, 5));
+        }while(kovPerm(nemzet, 5));
+    }while(kovPerm(szin, 5));
 
     return 0;
 }

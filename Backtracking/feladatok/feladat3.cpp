6f1afa6478a759315e1432621202180f0210a5c4
iffiuszabolcsmikes <iffiu.szabolcs@mikeslici.ro>
2026-06-12 09:32:46 +0300

    Create feladat3.cpp

---
+++ Backtracking/feladatok/feladat3.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+//verem merete
+const int N = 100;
+int verem[N];
+
+//lehetseges esetek szama
+int LE = 2;
+
+int k = 1;
+
+int n=45;
+int cimletek[10] = {3,5};
+
+
+int osszeg(int szint)
+{
+    int sum = 0;
+    for(int i = 0; i < szint; i++)
+        sum += verem[i];
+    return sum;
+}
+
+bool megoldas(int szint)
+{
+    return osszeg(szint ) == n;
+}
+
+bool jo(int szint)
+{
+    for(int i = 0; i < szint; i++)
+        if(verem[i] > verem[szint])
+            return false;
+    if(osszeg(szint)>n)
+        return false;
+    return true;
+}
+
+void kiir(int szint)
+{
+    verem[szint] = 0;
+    cout << k << ". " << endl;
+    for(int i = 0; i < szint; i++)
+        cout << verem[i] << " ";
+    cout << endl;
+    k++;
+}
+
+void backtracking(int szint)
+{
+    if(megoldas(szint))
+    {
+        kiir(szint);
+        return;
+    }
+
+    for(int i = 0; i < LE; i++)
+    {
+        verem[szint] = cimletek[i];
+        if(jo(szint))
+            backtracking(szint + 1);
+    }
+}
+
+
+int main()
+{
+    backtracking(0);
+    return 0;
+}
